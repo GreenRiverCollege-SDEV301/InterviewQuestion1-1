fa685e8d646215fc8325df9f428be20c899a9e0c
Everett Hanke <forgetekbusiness@gmail.com>
2024-01-09 10:50:17 -0800

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Hashtable;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -11,10 +13,27 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
-        System.out.println(isUnique("String"));
+        System.out.println("Is 'string' a unique word: "+isUnique("string")); //prints true
+        System.out.println("Is 'unique' a unique word: " + (isUnique("unique"))); //prints false
     }
-    public static boolean isUnique(String word)
+    public static boolean isUnique(String word) throws StringIndexOutOfBoundsException
     {
+        return isUnique(word, 0);
+    }
+    private static boolean isUnique(String word, int index) throws StringIndexOutOfBoundsException
+    {
+        char letter = word.charAt(index);
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (letter == word.charAt(i) && i != index)
+            {
+                return false;
+            }
+        }
+        if (letter == word.charAt(word.length()-1))
+        {
+            return isUnique(word, index+1);
+        }
         return true;
     }
 }
