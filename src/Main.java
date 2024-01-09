13d19f6a7ef60265aa09538356d4d30380b165f5
leinigoe <leinigoe@gmail.com>
2024-01-09 09:47:42 -0800

    Added isUnique method header

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,11 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        // quick test
+        System.out.println(isUnique("hello"));
+    }
+
+    public static boolean isUnique(String word) {
+        return false;
     }
 }
