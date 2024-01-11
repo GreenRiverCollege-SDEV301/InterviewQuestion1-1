87d916b5a24ff09296f6516f3a55de91c78d52e7
Cmatthews <cmatthews44@noreply.github.com>
2024-01-10 19:33:36 -0800

    Finished basic isUnique

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,38 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if (isUnique("always")) {
+            System.out.println("this is unqiue");
+        } else {
+            System.out.println("this is not unquie");
         }
     }
-    public static boolean isUnique(String word)
-    { return false;
-//test 2
+
+
+    public static boolean isUnique(String word) {
+
+        for (int i = 0; i <= word.length(); i++) {
+            char a = word.charAt(i);
+            for (int j = i + 1; j < word.length(); j++) {
+                if (word.charAt(j) == a) {
+
+                    return true;
+                }
+
+            }
+
+
+        }
+
+                return false;
+
+            }
+
+
+
     }
-}
+
+
+
+
+
